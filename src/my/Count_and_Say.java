a87cffe7dae8d15d5002ccab108ac8a428e1a384
jt0324 <774992009@qq.com>
2016-11-01 10:06:44 +0800

    2016.11.1

--- src/my/Count_and_Say.java
+++ src/my/Count_and_Say.java
@@ -6,7 +6,20 @@ package my;
 public class Count_and_Say {
     public String countAndSay(int n) {
         String[] seq = new String[n];
-
-        return "";
+        seq[0] = "1";
+        for (int i = 1;i < n ; ++i){
+            String str = seq[i - 1];
+            String temp = "";
+            for (int j = 0;j < str.length(); ++j){
+                int count = 1;
+                while (j < str.length() - 1 && str.charAt(j) == str.charAt(j + 1)){
+                    count++;
+                    j++;
+                }
+                temp += (count + "" + str.charAt(j));
+            }
+            seq[i] = temp;
+        }
+        return seq[n - 1];
     }
 }
