3006ab93e0436f89b2d82efb8d06c42e66adb9e7
jt0324 <774992009@qq.com>
2016-11-05 16:28:10 +0800

    2016.11.5

--- src/my/Triangle.java
+++ src/my/Triangle.java
@@ -1,6 +1,7 @@
 package my;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,17 +9,34 @@ import java.util.List;
  */
 public class Triangle {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[] minArray = new int[triangle.size()];
-        for (int i = 0;i < triangle.size(); ++i){
-            int min = Integer.MAX_VALUE;
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(triangle.get(0));
+        for (int i = 1;i < triangle.size(); ++i){
+            res.add(new ArrayList<Integer>());
             for (int j = 0;j < triangle.get(i).size(); ++j){
-                if (triangle.get(i).get(j) < min)
-                    min = triangle.get(i).get(j);
+                if (j == 0)
+                    res.get(i).add(triangle.get(i).get(j) + res.get(i - 1).get(j));
+                else if(j == triangle.get(i).size() - 1)
+                    res.get(i).add(triangle.get(i).get(j) + res.get(i - 1).get(j - 1));
+                else
+                    res.get(i).add(triangle.get(i).get(j) + Math.min(res.get(i - 1).get(j - 1),res.get(i - 1).get(j)));
             }
-            if (i == 0) minArray[0] = min;
-            else minArray[i] = min + minArray[i - 1];
         }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0;i < res.get(res.size() - 1).size(); ++i){
+            if (res.get(res.size() - 1).get(i) < min) min = res.get(res.size() - 1).get(i);
+        }
+        return min;
+    }
 
-        return minArray[minArray.length - 1];
+    public static void main(String[] args) {
+        Triangle t = new Triangle();
+        List<List<Integer>> list = new ArrayList<>();
+        Integer[] line2 = {2,3};
+        Integer[] line3 = {1,-1,3};
+        list.add(new ArrayList<Integer>(Arrays.asList(-1)));
+        list.add(new ArrayList<Integer>(Arrays.asList(line2)));
+        list.add(new ArrayList<Integer>(Arrays.asList(line3)));
+        t.minimumTotal(list);
     }
 }
